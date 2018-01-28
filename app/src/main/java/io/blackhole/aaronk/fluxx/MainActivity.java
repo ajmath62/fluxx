afdfb68275f695d626be28af1acf22a296343d07
Aaron Klein <ajmath62@gmail.com>
2018-01-28 10:21:30 -0500

    Split game up into stages
    This is modelled off what I did in the python version
    Also added a new game button to replace the "draw a card" button

--- app/src/main/java/io/blackhole/aaronk/fluxx/MainActivity.java
+++ app/src/main/java/io/blackhole/aaronk/fluxx/MainActivity.java
@@ -20,44 +20,29 @@ public class MainActivity extends AppCompatActivity {
     private List<Card> deck = new ArrayList<>();
     private Set<Card> hand = new TreeSet<>();
     private Set<Keeper> keepers = new TreeSet<>();
-    private Goal currentGoal = null;
+    private Goal currentGoal;
+    private GameState gameState;
+    private boolean gameOver;
+    private int cardsDrawn;
+    private int cardsPlayed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        Resources res = getResources();
-        String[] keeperList = res.getStringArray(R.array.keepers);
-        for (String k: keeperList) {
-            deck.add(new Keeper(this, k));
-        }
-
-        String[] goalList = res.getStringArray(R.array.goals);
-        for (String g: goalList) {
-            String[] goalDescription = g.split("~");
-            String name = goalDescription[0];
-            String[] goalDescriptionTail = Arrays.copyOfRange(goalDescription, 1, goalDescription.length);
-            // AJK TODO check why I'm using HashSet (here and elsewhere)
-            Set<String> requiredKeeperNames = new HashSet<>(Arrays.asList(goalDescriptionTail));
-            deck.add(new Goal(this, name, requiredKeeperNames));
-        }
     }
 
-    public void drawCard(View view) {
-        if (deck.isEmpty()) {
-            deck.addAll(hand);
-            hand.clear();
-        }
-        Card card = deck.remove(deck.size() - 1);
-        hand.add(card);
-        update();
+    public void newGame(View view) {
+        gameState = GameState.START_GAME;
+        gameOver = false;
+        processGameState();
     }
 
     public void playCard(Card card) {
-        if (!hand.contains(card)) {
+        if (gameState != GameState.PLAY)
             return;
-        }
+        if (!hand.contains(card))
+            return;
         Log.d("MainActivity", "Playing " + card.name);
         if (card instanceof Keeper) {
             hand.remove(card);
@@ -66,7 +51,85 @@ public class MainActivity extends AppCompatActivity {
         else if (card instanceof Goal) {
             // AJK TODO put currentGoal (if any) in the discard pile
             hand.remove(card);
-            this.currentGoal = (Goal) card;
+            currentGoal = (Goal) card;
+        }
+        cardsPlayed ++;
+
+        gameState = GameState.END_PLAY;
+        processGameState();
+    }
+
+    private void drawCard() {
+        if (deck.isEmpty()) {
+            deck.addAll(hand);
+            hand.clear();
+        }
+        Card card = deck.remove(deck.size() - 1);
+        hand.add(card);
+        cardsDrawn ++;
+    }
+
+    private void processGameState() {
+        if (gameState == GameState.START_GAME) {
+            // Remove all cards from the game
+            currentGoal = null;
+            deck.clear();
+            hand.clear();
+            keepers.clear();
+
+            // Add new cards to the deck
+            Resources res = getResources();
+            String[] keeperList = res.getStringArray(R.array.keepers);
+            for (String k: keeperList) {
+                deck.add(new Keeper(this, k));
+            }
+
+            String[] goalList = res.getStringArray(R.array.goals);
+            for (String g: goalList) {
+                String[] goalDescription = g.split("~");
+                String name = goalDescription[0];
+                String[] goalDescriptionTail = Arrays.copyOfRange(goalDescription, 1, goalDescription.length);
+                // AJK TODO check why I'm using HashSet (here and elsewhere)
+                Set<String> requiredKeeperNames = new HashSet<>(Arrays.asList(goalDescriptionTail));
+                deck.add(new Goal(this, name, requiredKeeperNames));
+            }
+
+            // Draw up a three-card hand
+            for (int i = 0; i < 3; i ++)
+                drawCard();
+
+            gameState = GameState.START_TURN;
+            processGameState();
+        }
+        else if (gameState == GameState.START_TURN) {
+            cardsDrawn = 0;
+            cardsPlayed = 0;
+            drawCard();
+            gameState = GameState.START_PLAY;
+            processGameState();
+        }
+        else if (gameState == GameState.START_PLAY) {
+            // AJK TODO maybe this state isn't necessary anymore
+            gameState = GameState.PLAY;
+        }
+        else if (gameState == GameState.PLAY) {
+            // Nothing to do here, just wait until a card is chosen
+        }
+        else if (gameState == GameState.END_PLAY) {
+            if (currentGoal.isSatisfied(keepers))
+                gameState = GameState.END_GAME;
+            else if (cardsPlayed < 1)
+                gameState = GameState.START_PLAY;
+            else
+                gameState = GameState.END_TURN;
+            processGameState();
+        }
+        else if (gameState == GameState.END_TURN) {
+            gameState = GameState.START_TURN;
+            processGameState();
+        }
+        else if (gameState == GameState.END_GAME) {
+            gameOver = true;
         }
         update();
     }
@@ -89,11 +152,13 @@ public class MainActivity extends AppCompatActivity {
         currentGoalView.removeAllViews();
         if (currentGoal != null) {
             currentGoalView.addView(currentGoal.view);
-            if (currentGoal.isSatisfied(keepers)) {
-                View youWin = findViewById(R.id.youWin);
-                youWin.setVisibility(View.VISIBLE);
-            }
         }
+
+        View youWin = findViewById(R.id.youWin);
+        if (gameOver)
+            youWin.setVisibility(View.VISIBLE);
+        else
+            youWin.setVisibility(View.INVISIBLE);
     }
 
     private class CardClick implements View.OnClickListener {
@@ -107,5 +172,9 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             playCard(card);
         }
+    }
+
+    private enum GameState {
+        START_GAME, START_TURN, START_PLAY, PLAY, END_PLAY, END_TURN, END_GAME
     }
 }
